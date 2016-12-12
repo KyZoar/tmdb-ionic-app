d080e8586456be7b55e04243d8615e84a311649a
KyZoar <marion.pellicer@laposte.net>
2016-12-12 16:24:29 +0100

    commit

--- src/modules/app.states/service.states.js
+++ src/modules/app.states/service.states.js
@@ -4,16 +4,35 @@
 (function (module) {
   'use strict';
 
-  function StatesService($q, httpService, i18nService) {
+  function StatesService($q, httpService, i18nService, API_IMAGES_URL, API_KEY) {
     var service = this;
 
     service.getMovie = function (id) {
-      return $q.resolve({ id: id, title: 'Movie', description: 'Awesome' });
+      return httpService.get('/3/movie/' + id, {
+        language: i18nService.getLocale(),
+        api_key: API_KEY
+      });
     };
 
     service.search = function (query) {
-      return $q.resolve([
-        { title: query, id: 1 }, { title: query, id: 2 }]);
+      return httpService.get('/3/search/movie', {
+        language: i18nService.getLocale(),
+        api_key: API_KEY,
+        query: query
+      }).then(function (data) {
+        return data.results;
+      });
+    };
+
+    service.discoverMovie = function () {
+      return httpService.get('/3/discover/movie', {
+        'release_date.lte': moment().add(3, 'months').format('YYYY-MM-DD'),
+        'release_dat.gte': moment().format('YYYY-MM-DD'),
+        language: i18nService.getLocale(),
+        api_key: API_KEY
+      }).then(function (data) {
+        return _.smaple(data.results) || $q.reject();
+      });
     };
 
     /**
@@ -32,6 +51,8 @@
     '$q',
     'httpService',
     'i18nService',
+    'API_IMAGES_URL',
+    'API_KEY',
     StatesService
   ]);
 
